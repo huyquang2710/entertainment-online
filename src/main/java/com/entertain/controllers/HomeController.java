c24261d226782be35d78608c6ff81c52bb7e195f
Huy Quang <huyquang271099@gmail.com>
2021-10-04 04:14:30 +0700

    add persistence.xml, test project

---
+++ src/main/java/com/entertain/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.entertain.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/index")
+public class HomeController extends HttpServlet{
+
+	private static final long serialVersionUID = -7131209992553490556L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/views/user/index.jsp");
+		requestDispatcher.forward(req, resp);
+	}
+
+}
