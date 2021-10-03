dc410ba5727b2eb65788ec842230e1ee7f3c9153
Huy Quang <huyquang271099@gmail.com>
2021-10-04 04:48:13 +0700

    add factory

---
+++ src/main/java/com/entertain/utils/JpaUtil.java
@@ -0,0 +1,21 @@
+package com.entertain.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+	private static EntityManagerFactory factory;
+	public static EntityManager getEntityManager() {
+		if(factory == null || factory.isOpen()) {
+			factory = Persistence.createEntityManagerFactory("entertainment");
+		}
+		return factory.createEntityManager();
+	}
+	public static void shutDown() {
+		if(factory != null || factory.isOpen()) {
+			factory.close();
+		}
+		factory = null;
+	}
+}
